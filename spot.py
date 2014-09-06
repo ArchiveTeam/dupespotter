ab56d1b52ee8ac7b1189e6ddc2bd100457a8c21a
Ivan Kozik <ivan@ludios.org>
2014-09-06 07:02:27 +0000

    Make it work

--- spot.py
+++ spot.py
@@ -2,9 +2,10 @@
 
 import sys
 import os
+import difflib
 
 from hashlib import md5
-from urllib.request import urlopen
+from urllib.request import urlopen, HTTPError
 
 cache_dir = "cache"
 
@@ -19,12 +20,17 @@ def get_body(url):
 		with open(fname, "rb") as f:
 			return f.read()
 	else:
-		with urlopen(url) as o:
-			body = o.read()
+		try:
+			with urlopen(url) as o:
+				body = o.read()
+				with open(fname, "wb") as f:
+					f.write(body)
+				return body
+		except HTTPError as e:
+			body = e.fp.read()
 			with open(fname, "wb") as f:
 				f.write(body)
 			return body
-
 
 def main():
 	try:
@@ -38,8 +44,17 @@ def main():
 		# Just save and print the body
 		print(get_body(sys.argv[1]))
 	elif len(sys.argv) == 3:
-		first = get_body(sys.argv[1])
-		second = get_body(sys.argv[2])
+		url1, url2 = sys.argv[1], sys.argv[2]
+		body1 = get_body(url1)
+		body2 = get_body(url2)
+		# TODO: handle non-utf-8 bodies
+		for line in difflib.unified_diff(
+			body1.decode("utf-8").splitlines(keepends=True),
+			body2.decode("utf-8").splitlines(keepends=True),
+			fromfile=url1,
+			tofile=url2):
+
+			sys.stdout.write(line)
 	else:
 		assert 0, sys.argv
 
